f784c1194b6fd3b3451a9d7c07c63b258ca20602
muhammaddonifirmansyah43-dotcom <muhammaddonifirmansyah43@gmail.com>
2026-06-09 15:41:07 +0700

    fix jobcard filename case for vercel

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import JobCard from "../components/Jobcard";
+import JobCard from "../components/JobCard";
 
 import api from "../api/api";
 import { mapInternshipToJob } from "../api/jobMapper";
